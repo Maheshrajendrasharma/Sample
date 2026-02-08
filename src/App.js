1a4ac2463faa83b6d94123e7808d34fbfc083430
Maheshrajendrasharma <117073189+Maheshrajendrasharma@users.noreply.github.com>
2026-02-08 21:03:19 +0530

    your clear message here

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function Layout() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
